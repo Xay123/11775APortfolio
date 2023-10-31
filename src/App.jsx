16228f83496537e6ba317d5c96fb4845aa2124e7
Zachary Layland <65696050+Xay123@users.noreply.github.com>
2023-10-31 10:22:17 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Hero, Navbar, Tech, StarsCanvas } from "./components";
+import { About, Contact, Experience, Hero, Navbar, Tech, StarsCanvas, Works, Feedbacks } from "./components";
 
 const App = () => {
   return (
